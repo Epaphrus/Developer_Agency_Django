0f5e39dffaf0fbbde7de72d8c4b4bb8023a791f3
Maxwel .E. Khadambi <83645259+Epaphrus@users.noreply.github.com>
2023-10-30 19:11:16 +0300

    urls

--- Developer_Agency_Django/urls.py
+++ Developer_Agency_Django/urls.py
@@ -23,5 +23,6 @@ urlpatterns = [
     path('', views.home, name='homepage'),
     path('about/', views.about, name='about'),
     path('contact/', views.contact, name='contact'),
+    path('whatwedo/', views.whatwedo, name='whatwedo'),
 
 ]
