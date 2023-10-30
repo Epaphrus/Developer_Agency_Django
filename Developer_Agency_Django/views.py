8f358999dc238574d935a996478f8216796a5cf1
Maxwel .E. Khadambi <83645259+Epaphrus@users.noreply.github.com>
2023-10-30 19:04:47 +0300

    views

--- Developer_Agency_Django/views.py
+++ Developer_Agency_Django/views.py
@@ -1,4 +1,17 @@
 from django.shortcuts import render
 
+
 def home(request):
     return render(request, 'index.html')
+
+
+def about(request):
+    return render(request, 'about.html')
+
+
+def whatwedo(request):
+    return render(request, 'do.html')
+
+
+def contact(request):
+    return render(request, '')
